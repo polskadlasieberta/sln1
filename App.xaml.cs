42259edd0f398c836b916f59b02f80fd0f7fb17a
mczap123 <mg0610765@gmail.com>
2026-01-08 02:06:37 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace michal
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace michal
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
